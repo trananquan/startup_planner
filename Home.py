bda8e7e11eff8b7cd35118c5b404acfa94a7ec6d
trananquan <130271615+trananquan@users.noreply.github.com>
2026-02-08 12:10:49 +0700

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Startup Planning Toolkit",
+    layout="wide"
+)
+
+st.title("🚀 Phần mềm hỗ trợ lập kế hoạch và báo cáo cho Start-up và SME ")
+
+st.markdown("""
+Ứng dụng hỗ trợ **Startup & SME** trong:
+- 📊 Tính toán và lập kế hoạch tài chính
+- 📈 Tính toán và lập kế hoạch kinh doanh 
+- 📈 Phân tích chiến lược, theo dõi KPI và cảnh báo rủi ro
+- 🧾 Xuất ra báo cáo phân tích tài chính và kinh doanh
+
+Chọn module ở thanh bên trái.
+""")
+st.image("img.jpg")
+
